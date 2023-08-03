2768a1dc5ddb1a4b80c8b37b357aa7e0d6b1b460
shinymoon99 <2686288339@qq.com>
2023-08-03 19:09:51 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+import json
+
+with open("procedure_flowgraph.json","r",encoding="utf-8") as f:
+    ICTdata = json.load(f)
+train_data = ICTdata[:int(0.8*(len(ICTdata)))]
+eval_data = ICTdata[int(0.8*(len(ICTdata))):]
+with open("./datasets/ICT_train.json","w",encoding="utf-8") as f1:
+    json.dump(train_data,f1,ensure_ascii=False)
+with open("./datasets/ICT_dev.json","w",encoding="utf-8") as f2:
+    json.dump(eval_data,f2,ensure_ascii=False)
